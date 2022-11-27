00173bb7723bd643868bbb3ce732ed58b571912f
Julia <julia@yakovenko.ru>
2022-11-27 09:10:49 +0300

    H/w TASK 27

--- Homework4/Program.cs
+++ Homework4/Program.cs
@@ -23,7 +23,7 @@ ShowArray(numbers);*/
 
 /*Задача 25. Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.*/
 
-int Exponentation (int a, int b)
+/*int Exponentation (int a, int b)
 {
     int res = a;
     for(int count = 1; count < b; count++)
@@ -38,5 +38,27 @@ int user_A = Convert.ToInt32(Console.ReadLine());
 Console.Write("Input B: ");
 int user_B = Convert.ToInt32(Console.ReadLine());
 int result = Exponentation(user_A, user_B);
-Console.WriteLine($"{user_A} ^ {user_B} = {result}.");
+Console.WriteLine($"{user_A} ^ {user_B} = {result}.");*/
 
+
+/*Задача 27. Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.*/
+
+int SumOfDigits (int num)
+{
+    int sum = 0;
+    int current = 0;
+    int digit;
+    while (num > 0)
+    {
+        digit = num % 10;
+        sum += digit;
+        num = num / 10;
+        current ++;
+    }
+    return sum;
+}
+
+Console.Write("Input you number: ");
+int user_num = Convert.ToInt32(Console.ReadLine());
+int result = SumOfDigits(user_num);
+Console.WriteLine($"Sum of digits in your number {user_num} is {result}.");
