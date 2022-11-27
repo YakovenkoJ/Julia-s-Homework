369c05e905cbe5012e039b9194ff544d870413d0
Julia <julia@yakovenko.ru>
2022-11-27 08:41:47 +0300

    H/w TASK 25

--- Homework4/Program.cs
+++ Homework4/Program.cs
@@ -1,6 +1,6 @@
 ﻿/*Задача 29. Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.*/
 
-int [] CreateNewArray (int size)
+/*int [] CreateNewArray (int size)
 {
     int [] array = new int [size];
     for (int i = 0; i < size; i++)
@@ -18,4 +18,25 @@ void ShowArray (int [] array)
 
 int array_size = 8;
 int[] numbers = CreateNewArray (array_size);
-ShowArray(numbers);
+ShowArray(numbers);*/
+
+
+/*Задача 25. Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.*/
+
+int Exponentation (int a, int b)
+{
+    int res = a;
+    for(int count = 1; count < b; count++)
+    {
+        res = res * a;
+    }
+    return res;
+}
+
+Console.Write("Input A: ");
+int user_A = Convert.ToInt32(Console.ReadLine());
+Console.Write("Input B: ");
+int user_B = Convert.ToInt32(Console.ReadLine());
+int result = Exponentation(user_A, user_B);
+Console.WriteLine($"{user_A} ^ {user_B} = {result}.");
+
