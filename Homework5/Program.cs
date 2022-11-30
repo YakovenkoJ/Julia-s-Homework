6d08b2ce9b41fa2480b0e33d7518d66658ed6bd3
Julia <julia@yakovenko.ru>
2022-11-30 16:31:00 +0300

    H/w TASK 38

--- Homework5/Program.cs
+++ Homework5/Program.cs
@@ -38,7 +38,7 @@ Console.WriteLine($"Count of even numbers is {result}.");*/
 /*Задача 36. Задайте одномерный массив, заполненный случайными числами. Найдите сумму элементов, стоящих на нечётных позициях.*/
 
 
-int [] CreateRandomArray (int size, int minValue, int maxValue)
+/*int [] CreateRandomArray (int size, int minValue, int maxValue)
 {
     int [] array = new int [size];
     for (int i = 0; i < size; i++)
@@ -72,5 +72,50 @@ int maxVal = Convert.ToInt32(Console.ReadLine());
 int [] numbers = CreateRandomArray(size_array, minVal, maxVal);
 ShowArray(numbers);
 int result = Sum(numbers);
-Console.Write($"Sum of elements on even indexes is {result}.");
+Console.Write($"Sum of elements on even indexes is {result}.");*/
 
+
+/*Задача 38. Задайте массив вещественных чисел. Найдите разницу между максимальным и минимальным элементов массива.*/
+
+double [] CreateRandomArray (int size)
+{
+    double [] array = new double [size];
+    for (int i = 0; i < size; i++)
+        array[i] = new Random().Next(1,100) + Math.Round(new Random().NextDouble(),3);
+    return array;
+}
+
+
+void ShowArray (double [] array)
+{
+    for (int i = 0; i < array.Length; i++)
+        Console.Write(array[i] + " ");
+    Console.WriteLine();
+}
+
+double Difference (double [] array)
+{
+    double min = array [0];
+    double max = array [1];
+    double result;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array [i] < min)
+        {
+            min = array [i];
+        }
+        else if (array [i] > max)
+        {
+            max = array[i];
+        }
+    }
+    result = max - min;
+    return result;
+}
+
+int size_array = 5;
+Console.Write("   ");
+double [] numbers = CreateRandomArray(size_array);
+ShowArray(numbers);
+double res = Math.Round(Difference(numbers), 3);
+Console.WriteLine($"Difference b/w max & min element of array is {res}.");
