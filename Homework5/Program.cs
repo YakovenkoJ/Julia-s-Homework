b800f305c0bc5a441a80ad7f43545ff97a4391ed
Julia <julia@yakovenko.ru>
2022-11-30 16:24:49 +0300

    H/w TASK 36

--- Homework5/Program.cs
+++ Homework5/Program.cs
@@ -2,7 +2,7 @@
 Напишите программу, которая покажет количество чётных чисел в массиве.*/
 
 
-int [] CreateRandomArray (int size)
+/*int [] CreateRandomArray (int size)
 {
     int [] array = new int [size];
     for (int i = 0; i < size; i++)
@@ -32,4 +32,45 @@ Console.Write(" ");
 int [] numbers = CreateRandomArray (qnt);
 ShowArray(numbers);
 int result = CountEvenNumber(numbers);
-Console.WriteLine($"Count of even numbers is {result}.");
+Console.WriteLine($"Count of even numbers is {result}.");*/
+
+
+/*Задача 36. Задайте одномерный массив, заполненный случайными числами. Найдите сумму элементов, стоящих на нечётных позициях.*/
+
+
+int [] CreateRandomArray (int size, int minValue, int maxValue)
+{
+    int [] array = new int [size];
+    for (int i = 0; i < size; i++)
+        array[i] = new Random().Next(minValue, maxValue + 1);
+    return array;
+}
+
+
+void ShowArray (int [] array)
+{
+    for (int i = 0; i < array.Length; i++)
+        Console.Write(array[i] + " ");
+    Console.WriteLine();
+}
+
+int Sum (int [] array)
+{
+    int sum = 0;
+    for (int i = 0; i < array.Length; i++)
+        if (i % 2 == 0) sum += array [i];
+    return sum;
+}
+
+Console.Write("Input qnt of elements in array: ");
+int size_array = Convert.ToInt32(Console.ReadLine());
+Console.Write("Input min value of array: ");
+int minVal= Convert.ToInt32(Console.ReadLine());
+Console.Write("Input max value of array: ");
+int maxVal = Convert.ToInt32(Console.ReadLine());
+
+int [] numbers = CreateRandomArray(size_array, minVal, maxVal);
+ShowArray(numbers);
+int result = Sum(numbers);
+Console.Write($"Sum of elements on even indexes is {result}.");
+
