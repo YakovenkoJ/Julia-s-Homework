f53f5e84e7127e9f679ad2fe029c744db9d72a2a
Julia <julia@yakovenko.ru>
2022-12-03 16:20:21 +0300

    H/w TASK 41

--- Homework6/Program.cs
+++ Homework6/Program.cs
@@ -1,2 +1,39 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/*Задача 41. Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.*/
+
+int[] CreateArray()
+{
+    Console.Write("Input qnt of elements in array: ");
+    int M = Convert.ToInt32(Console.ReadLine());
+    int[] array = new int[M];
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"Input element {i} of array: ");
+        array[i] = Convert.ToInt32(Console.ReadLine());
+    }
+    return array;
+}
+
+
+void ShowArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+        Console.Write(array[i] + " ");
+    Console.WriteLine();
+}
+
+
+int CountPos(int[] array)
+{
+    int sum = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] > 0)
+            sum += array[i];
+    }
+    return sum;
+}
+
+int [] numbers = CreateArray();
+ShowArray(numbers);
+int result = CountPos(numbers);
+Console.Write($"Sum of elements > 0 is {result}.");
