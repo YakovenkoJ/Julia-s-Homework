23ed7e7ef798852a91dda077e0fd42c64bcbb687
Julia <julia@yakovenko.ru>
2022-12-15 22:17:07 +0300

    H/w TASK 66

--- Homework9/Program.cs
+++ Homework9/Program.cs
@@ -36,7 +36,7 @@ Console.WriteLine($"Your number {number} in binar system is {result}.");*/
 /*Задача 68 (дополнительно). Напишите программу вычисления функции Аккермана с помощью рекурсии. 
 Даны два неотрицательных числа m и n.*/
 
-int A(int n, int m)
+/*int A(int n, int m)
 {
     if (n == 0) return m + 1;
     else if (m == 0) return A(n - 1, 1);
@@ -50,4 +50,26 @@ Console.Write("Input 2nd positive number. Your number should be 0 >= number < 4:
 int M = Convert.ToInt32(Console.ReadLine());
 int res = A(N, M);
 Console.WriteLine();
-Console.Write($"A(n,m) = {res}.");
+Console.Write($"A(n,m) = {res}.");*/
+
+
+/*Задача 66. Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.*/
+
+int Sum(int M, int N)
+{
+    int sum = 0;
+    if (M > N) return 0;
+    else return sum = (M + Sum(M + 1, N));
+}
+
+Console.Write("Input 1st positive number: ");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.Write("Input 1st positive number: ");
+int n = Convert.ToInt32(Console.ReadLine());
+if (m >= n) Console.Write("Incorrect input! 1st number should be < 2nd number.");
+else
+{
+    int res = Sum(m, n);
+    Console.Write($"Sum of elements b/w {m} & {n} is {res}.");
+}
+
